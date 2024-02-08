418f95ffe0ab9349a6f10bdad971bc11d44b08ab
JunglTemple <tubikardu@gmail.com>
2024-02-08 13:02:02 +0000

    Added a bunch of stuff

--- interwebs/pages/index.vue
+++ interwebs/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-	<div class="m-6 mt-24">
-		<h1 class="font-title text-9xl"><b>PROJECT {INTERWEBS}</b></h1>
+	<div class="m-6 mt-24" id="MAIN">
+		<h1 class="font-title text-9xl mb-2 text-primary">
+			<b>PROJECT {INTERWEBS}</b>
+		</h1>
+		<p class="text-2xl">Get your point inserted today!</p>
 	</div>
 </template>
---
+++ interwebs/pages/teamsmp.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="m-6 mt-24" id="MAIN">
+		<span class="text-cool block text-xl">Point</span>
+		<h1 class="font-title text-9xl mb-2 text-green-500">
+			<b>Team SMP</b>
+		</h1>
+    <p>The Team SMP is a survival Minecraft server, run by XxDreamXxXx and JunglTemple.</p>
+	</div>
+</template>
