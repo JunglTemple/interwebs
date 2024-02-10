83edb556f8c5b3e34f24f0d6ff963ddcbec3bc53
JunglTemple <tubikardu@gmail.com>
2024-02-10 17:27:04 +0000

    Added some stuff...

--- interwebs/pages/index.vue
+++ interwebs/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="m-6 mt-24" id="MAIN">
-		<h1 class="font-title text-9xl mb-2 text-primary">
-			PROJECT <b>{INTERWEBS}</b>
-		</h1>
+		<div id="title-block">
+			<h1 class="font-title text-9xl mb-2 text-primary">
+				PROJECT <b>{INTERWEBS}</b>
+			</h1>
+		</div>
 		<p class="text-2xl">
 			Open a <b class="text-primary">Point</b> using the button at the top of
 			the screen!
--- interwebs/pages/teamsmp.vue
+++ interwebs/pages/teamsmp.vue
@@ -1,9 +1,14 @@
 <template>
 	<div class="m-6 mt-24" id="MAIN">
-		<span class="text-cool block text-xl">Point</span>
-		<h1 class="font-title text-9xl mb-2 text-green-500">
-			<b>Team SMP</b>
-		</h1>
+		<div
+			id="title-block"
+			class="bg-[url('/img/teamsmp/lobby-2-shaders.png')] p-5 mb-3 rounded-3xl bg-center bg-cover"
+		>
+			<span class="text-cool block text-xl">Point</span>
+			<h1 class="font-title text-9xl text-green-500">
+				<b>Team SMP</b>
+			</h1>
+		</div>
 		<p class="text-2xl">
 			The Team SMP is a survival Minecraft server, run by XxDreamXxXx and
 			JunglTemple.
